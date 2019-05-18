b90a7a81265391d672e83d799e11a9f360436512
Brenda Herrada <bherrada@summasolutions.net>
2019-05-17 20:01:21 -0600

    Cambio a owl carousel viejo

--- index.php
+++ index.php
@@ -19,29 +19,25 @@
 		<script type="text/javascript" src="./files/jquery.min.js"></script>
 
 		<!-- Owl Carousel 2 JS -->
-		<script type="text/javascript" src="./vendor/owl-carousel/owl.carousel.min.js"></script>
+		<!-- <script type="text/javascript" src="./vendor/owl-carousel/owl.carousel.min.js"></script> -->
 		<!-- Old Owl Carousel JS -->
-		<!-- <script type="text/javascript" src="./files/owl.carousel.custom.js"></script> -->
+		<script type="text/javascript" src="./files/owl.carousel.custom.js"></script>
 	</head>
 
 	<body class="page-homepage pageType-ContentPage template-pages-layout-HomePageCustom pageLabel-homepage language-es">
 		<div class="slider_component simple-banner carousel-inner">
 			<div id="homepage_slider" class="margin-carrousel owl-theme owl-loaded">
-				<div class="owl-stage-outer">
-					<div class="owl-stage">
-						<div class="owl-item">
-							<a href="https://hybris.naldo.com.ar/"><img src="./files/Web-1440x350-ElectroFest.jpg" alt="Web 1440x350 - ElectroFest.jpg" title="Web 1440x350 - ElectroFest.jpg"></a>
-						</div>
-						<div class="owl-item">
-							<a href="https://hybris.naldo.com.ar/GENERAL/TECNOLOGIA/Telefonia/Celulares/c/120?q=%3Arelevance%3AmanufacturerName%3AMOTOROLA"><img src="./files/Web-1440x350-MotoSale.jpg" alt="Web 1440x350 - MotoSale.jpg" title="Web 1440x350 - MotoSale.jpg"></a>
-						</div>
-						<div class="owl-item">
-							<a href="https://hybris.naldo.com.ar/"><img src="./files/Web-1440x350-03.jpg" alt="Web 1440x350 - 03.jpg" title="Web 1440x350 - 03.jpg"></a>
-						</div>
-						<div class="owl-item">
-							<a href="https://hybris.naldo.com.ar/"><img src="./files/Web-1440x350-01.jpg" alt="Web 1440x350 - 01.jpg" title="Web 1440x350 - 01.jpg"></a>
-						</div>
-					</div>
+				<div class="item">
+					<a href="https://hybris.naldo.com.ar/"><img src="./files/Web-1440x350-ElectroFest.jpg" alt="Web 1440x350 - ElectroFest.jpg" title="Web 1440x350 - ElectroFest.jpg"></a>
+				</div>
+				<div class="item">
+					<a href="https://hybris.naldo.com.ar/GENERAL/TECNOLOGIA/Telefonia/Celulares/c/120?q=%3Arelevance%3AmanufacturerName%3AMOTOROLA"><img src="./files/Web-1440x350-MotoSale.jpg" alt="Web 1440x350 - MotoSale.jpg" title="Web 1440x350 - MotoSale.jpg"></a>
+				</div>
+				<div class="item">
+					<a href="https://hybris.naldo.com.ar/"><img src="./files/Web-1440x350-03.jpg" alt="Web 1440x350 - 03.jpg" title="Web 1440x350 - 03.jpg"></a>
+				</div>
+				<div class="item">
+					<a href="https://hybris.naldo.com.ar/"><img src="./files/Web-1440x350-01.jpg" alt="Web 1440x350 - 01.jpg" title="Web 1440x350 - 01.jpg"></a>
 				</div>
 			</div>
 		</div>
@@ -52,69 +48,65 @@
 						<div class="col-md-12">
 							<div class="yCmsContentSlot">
 								<div class="carousel__component" style="margin: 60px 0;">
-									<div class="owl-carousel owl-theme owl-loaded">
-										<div class="owl-stage-outer">
-											<div class="owl-stage">
-												<div class="owl-item">
-													<a href="https://hybris.naldo.com.ar/GENERAL/TECNOLOGIA/Televisores-y-Video/Tv-Led-y-Smart-TV/Smart-TV-Noblex-50%22-EA50X6100-Prueba/p/503576">
-														<div class="carousel__item has-reg-price">
-															<div class="carousel__item--thumb product-img img-responsive">
-																<img id="productImageConsultarBtn_503576" class="" src="./files/503576.jpg-300Wx300H" alt="Smart TV Noblex 50&quot; EA50X6100 Prueba" title="Smart TV Noblex 50&quot; EA50X6100 Prueba">
-															</div>
-															<p class="product-brand">NOBLEX</p>
+									<div class="new-owl-carousel owl-theme owl-loaded">
+										<div class="item">
+											<a href="https://hybris.naldo.com.ar/GENERAL/TECNOLOGIA/Televisores-y-Video/Tv-Led-y-Smart-TV/Smart-TV-Noblex-50%22-EA50X6100-Prueba/p/503576">
+												<div class="carousel__item has-reg-price">
+													<div class="carousel__item--thumb product-img img-responsive">
+														<img id="productImageConsultarBtn_503576" class="" src="./files/503576.jpg-300Wx300H" alt="Smart TV Noblex 50&quot; EA50X6100 Prueba" title="Smart TV Noblex 50&quot; EA50X6100 Prueba">
+													</div>
+													<p class="product-brand">NOBLEX</p>
 
-															<h3 class="product-title">
-																Smart TV Noblex 50" ...</h3>
-															<div class="price-box">
-																<!-- Para el precio en caso que haya una promocion para el producto la mostramos -->
-																<p id="product-price" class="product-price ">$21.000</p>
-																<hr class="product-divisor">
-																<form id="addToCartForm503576" class="add_to_cart_form" action="https://hybris.naldo.com.ar/cart/add" method="post"><input type="hidden" name="productCodePost" value="503576">
-																	<input type="hidden" name="productNamePost" value="Smart TV Noblex 50&quot; EA50X6100 Prueba">
-																	<input type="hidden" name="productPostPrice" value="21000.0">
-																	<button type="submit" class="btn btn-naldo js-enable-btn">Agregar</button>
-																	<div>
-																		<input type="hidden" name="CSRFToken" value="59f47413-6856-4fa0-98e6-3bbc8b2d47c2">
-																	</div>
-																</form>
+													<h3 class="product-title">
+														Smart TV Noblex 50" ...</h3>
+													<div class="price-box">
+														<!-- Para el precio en caso que haya una promocion para el producto la mostramos -->
+														<p id="product-price" class="product-price ">$21.000</p>
+														<hr class="product-divisor">
+														<form id="addToCartForm503576" class="add_to_cart_form" action="https://hybris.naldo.com.ar/cart/add" method="post"><input type="hidden" name="productCodePost" value="503576">
+															<input type="hidden" name="productNamePost" value="Smart TV Noblex 50&quot; EA50X6100 Prueba">
+															<input type="hidden" name="productPostPrice" value="21000.0">
+															<button type="submit" class="btn btn-naldo js-enable-btn">Agregar</button>
+															<div>
+																<input type="hidden" name="CSRFToken" value="59f47413-6856-4fa0-98e6-3bbc8b2d47c2">
 															</div>
-														</div>
-													</a>
+														</form>
+													</div>
 												</div>
-												<?php for($i=0; $i<10;$i++){ ?>
-												<div class="owl-item">
-													<a href="https://hybris.naldo.com.ar/GENERAL/TECNOLOGIA/Televisores-y-Video/Tv-Led-y-Smart-TV/Smart-TV-Noblex-50%22-EA50X6100-Prueba/p/503576">
-														<div class="carousel__item">
-															<div class="carousel__item--thumb product-img img-responsive">
-																<div class="cucar_ahorro"><p class="cucarda-sub">ahorro</p><p class="monto"><span class="signo">$</span><span>4.461</span></p></div>
-																<!-- <div class="cucar_ahora" style="background-image: url(https://hybris.naldo.com.ar/_ui/responsive/theme-naldo/images/products/cucarda_ahora12.png);"></div> -->
-																<img id="productImageConsultarBtn_503576" class="" src="./files/503576.jpg-300Wx300H" alt="Smart TV Noblex 50&quot; EA50X6100 Prueba" title="Smart TV Noblex 50&quot; EA50X6100 Prueba">
-															</div>
-															<p class="product-brand">NOBLEX</p>
-
-															<h3 class="product-title">
-																Smart TV Noblex 50" ...</h3>
-															<div class="price-box">
-																<!-- Para el precio en caso que haya una promocion para el producto la mostramos -->
-																<p id="product-old-price" class="product-old-price">$21.000</p>
-																<p id="discount-percentage" class="label label-success product-label">20%OFF</p>
-																<p id="product-price" class="product-price ">$16.539</p>
-																<hr class="product-divisor">
-																<form id="addToCartForm503576" class="add_to_cart_form" action="https://hybris.naldo.com.ar/cart/add" method="post"><input type="hidden" name="productCodePost" value="503576">
-																	<input type="hidden" name="productNamePost" value="Smart TV Noblex 50&quot; EA50X6100 Prueba">
-																	<input type="hidden" name="productPostPrice" value="21000.0">
-																	<button type="submit" class="btn btn-naldo js-enable-btn">Agregar</button>
-																	<div>
-																		<input type="hidden" name="CSRFToken" value="59f47413-6856-4fa0-98e6-3bbc8b2d47c2">
-																	</div>
-																</form>
-															</div>
-														</div>
-													</a>
-												</div>
-												<?php } ?>
-											</div>
+											</a>
 										</div>
+										<?php for($i=0; $i<10;$i++){ ?>
+										<div class="item">
+											<a href="https://hybris.naldo.com.ar/GENERAL/TECNOLOGIA/Televisores-y-Video/Tv-Led-y-Smart-TV/Smart-TV-Noblex-50%22-EA50X6100-Prueba/p/503576">
+												<div class="carousel__item">
+													<div class="carousel__item--thumb product-img img-responsive">
+														<div class="cucar_ahorro"><p class="cucarda-sub">ahorro</p><p class="monto"><span class="signo">$</span><span>4.461</span></p></div>
+														<!-- <div class="cucar_ahora" style="background-image: url(https://hybris.naldo.com.ar/_ui/responsive/theme-naldo/images/products/cucarda_ahora12.png);"></div> -->
+														<img id="productImageConsultarBtn_503576" class="" src="./files/503576.jpg-300Wx300H" alt="Smart TV Noblex 50&quot; EA50X6100 Prueba" title="Smart TV Noblex 50&quot; EA50X6100 Prueba">
+													</div>
+													<p class="product-brand">NOBLEX</p>
+
+													<h3 class="product-title">
+														Smart TV Noblex 50" ...</h3>
+													<div class="price-box">
+														<!-- Para el precio en caso que haya una promocion para el producto la mostramos -->
+														<p id="product-old-price" class="product-old-price">$21.000</p>
+														<p id="discount-percentage" class="label label-success product-label">20%OFF</p>
+														<p id="product-price" class="product-price ">$16.539</p>
+														<hr class="product-divisor">
+														<form id="addToCartForm503576" class="add_to_cart_form" action="https://hybris.naldo.com.ar/cart/add" method="post"><input type="hidden" name="productCodePost" value="503576">
+															<input type="hidden" name="productNamePost" value="Smart TV Noblex 50&quot; EA50X6100 Prueba">
+															<input type="hidden" name="productPostPrice" value="21000.0">
+															<button type="submit" class="btn btn-naldo js-enable-btn">Agregar</button>
+															<div>
+																<input type="hidden" name="CSRFToken" value="59f47413-6856-4fa0-98e6-3bbc8b2d47c2">
+															</div>
+														</form>
+													</div>
+												</div>
+											</a>
+										</div>
+										<?php } ?>
 									</div>
 									<style>
 
@@ -226,39 +218,33 @@
 		</section>
 		<script>
 			$(document).ready(function(){
-				$(".owl-carousel").owlCarousel({
-					nav: true,
-					dots: false,
+				$(".new-owl-carousel").owlCarousel({
+					navigation: true,
+					navigationText : ["‹", "›"],
+					pagination: false,
 					items: 4,
-					margin: 15,
-					responsive : {
-						0 : {
-							items: 1
-						},
-						480 : {
-							items: 2
-						},
-						640 : {
-							items: 3
-						},
-						960 : {
-							items: 4
-						}
-					},
+					itemsDesktop: false,
+					itemsDesktopSmall: [900, 3],
+					itemsTablet: [768, 3],
+					itemsTabletSmall: [767, 2],
+					itemsMobile: [479, 1],
 					autoHeight: false,
-					slideTransition: 'ease-out',
-					smartSpeed: 600,
-					navSpeed: 600,
+					slideSpeed: 600,
+					paginationSpeed: 600,
+					rewindSpeed: 1000,
 					loop: true
 				});
 				$("#homepage_slider").owlCarousel({
 					items: 1,
-					nav: true,
-					dots: true,
+					singleItem: true,
+					navigation: true,
+					navigationText : ["", ""],
+					pagination: true,
 					autoPlay: true,
-					slideTransition: 'ease-out',
-					smartSpeed: 600,
-					navSpeed: 600,
+					stopOnHover: true,
+					slideSpeed: 600,
+					paginationSpeed: 600,
+					rewindSpeed: 1000,
 					loop: true
 				});
 			});
